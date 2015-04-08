0738857b5c4f8e5f0d5df4ea28125ab5bd73b504
transistor <trans@jabberwocky.ca>
2015-04-07 23:41:51 -0700

    Added start of aggregator module

---
+++ nerve/aggregator/youtube.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import nerve
+
+class YouTubeAggregator (nerve.Device):
+    pass
+
+"""
+
+you could use these drivers to make data accessible.  It would be possible to use this mechanism to get the latest youtube recommendations
+for example, in some kind of data format.
+
+This whole module is supposed to be about making data from other websites accessible to the system, possibly for the sake of datalogging,
+or more likely, to display it in a special webpage, like a summary of the day, or integrating the latest whatever into... something
+
+"""
+
+ 
