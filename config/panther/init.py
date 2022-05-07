6f35f1cebdd82011035176b75d3ff061d484714f
transistor <trans@jabberwocky.ca>
2022-05-06 19:55:31 -0700

    Added Onkyo stereo to replace the Sony

--- config/panther/init.py
+++ config/panther/init.py
@@ -4,39 +4,110 @@
 import nerve
 import os
 
-class Stereo (nerve.Device):
+
+class OnkyoStereo (nerve.Device):
     def __init__(self, serial, **config):
         nerve.Device.__init__(self, **config)
         self.serial = serial
 
     def power(self):
-        #self.serial.send("ir S 0xA81")
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4B36D32C)
+
+    def volup(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB640BF)
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB640BF)
+
+    def voldown(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB6C03F)
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB6C03F)
+
+    def mute(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB6A05F)
+
+    def tape(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB610EF)
+
+    def tuner(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4BB6D02F)
+
+    def speaker_ab(self):
+        nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x4B3611EE)
+
+
+# 1 - 0x010
+# 2 - 0x810
+# 3 - 0x410
+# 4 - 0xC10
+# 5 - 0x210
+# 6 - 0xA10
+# 7 - 0x610
+# 8 - 0xE10
+# 9 - 0x110
+# 0 - 0x910
+class SonyTelevision (nerve.Device):
+    def __init__(self, serial, **config):
+        nerve.Device.__init__(self, **config)
+        self.serial = serial
+
+    def power(self):
+        self.serial.send("ir S 0xA90")
+
+    def volup(self):
+        self.serial.send("ir S 0x490")
+
+    def voldown(self):
+        self.serial.send("ir S 0xC90")
+
+    def mute(self):
+        self.serial.send("ir S 0x290")
+
+    def input(self):
+        self.serial.send("ir S 0xA50")
+
+    def ps3(self):
+        self.serial.send("ir S 0xA50")
+        self.serial.send("ir S 0x010")
+
+    def netbook(self):
+        self.serial.send("ir S 0xA50")
+        self.serial.send("ir S 0x810")
+
+    def computer(self):
+        self.serial.send("ir S 0xA50")
+        self.serial.send("ir S 0x410")
+
+    def appletv(self):
+        self.serial.send("ir S 0xA50")
+        self.serial.send("ir S 0xC10")
+
+
+class SonyStereo (nerve.Device):
+    def __init__(self, serial, **config):
+        nerve.Device.__init__(self, **config)
+        self.serial = serial
+
+    def power(self):
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0xA81)
 
     def volup(self):
-        #self.serial.send("ir S 0x481")
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x481)
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x481)
 
     def voldown(self):
-        #self.serial.send("ir S 0xC81")
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0xC81)
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0xC81)
 
     def mute(self):
-        #self.serial.send("ir S 0xC41")
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x281)
 
     def tape(self):
-        #self.serial.send("ir S 0xC41")
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0xC41)
 
     def tuner(self):
-        #self.serial.send("ir S 0x841")
         nerve.query("/devices/mysensors/11/1/send_msg", 32, 0x841)
 
 
-class Television (nerve.Device):
+class PanasonicTelevision (nerve.Device):
     def __init__(self, serial, **config):
         nerve.Device.__init__(self, **config)
         self.serial = serial
@@ -71,7 +142,7 @@ class Television (nerve.Device):
 
 
 rgb = nerve.get_object('/devices/rgb')
-nerve.set_object('/devices/stereo', Stereo(rgb))
-nerve.set_object('/devices/tv', Television(rgb))
+nerve.set_object('/devices/stereo', OnkyoStereo(rgb))
+nerve.set_object('/devices/tv', SonyTelevision(rgb))
 
 
